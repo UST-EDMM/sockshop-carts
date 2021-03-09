214d7fdb19d5db510a21efa0678e057952904d72
Michael Wurster <miwurster@googlemail.com>
2021-03-09 09:34:49 +0100

    Enable EDMM deployment

--- src/main/java/works/weave/socks/cart/CartApplication.java
+++ src/main/java/works/weave/socks/cart/CartApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnablePrometheusEndpoint
 public class CartApplication {
     public static void main(String[] args) {
         SpringApplication.run(CartApplication.class, args);
--- src/main/java/works/weave/socks/cart/configuration/WebMvcConfig.java
+++ src/main/java/works/weave/socks/cart/configuration/WebMvcConfig.java
@@ -7,13 +7,14 @@ import works.weave.socks.cart.middleware.HTTPMonitoringInterceptor;
 
 @Configuration
 public class WebMvcConfig {
-    @Bean
-    HTTPMonitoringInterceptor httpMonitoringInterceptor() {
-        return new HTTPMonitoringInterceptor();
-    }
 
-    @Bean
-    public MappedInterceptor myMappedInterceptor(HTTPMonitoringInterceptor interceptor) {
-        return new MappedInterceptor(new String[]{"/**"}, interceptor);
-    }
+//    @Bean
+//    HTTPMonitoringInterceptor httpMonitoringInterceptor() {
+//        return new HTTPMonitoringInterceptor();
+//    }
+
+//    @Bean
+//    public MappedInterceptor myMappedInterceptor(HTTPMonitoringInterceptor interceptor) {
+//        return new MappedInterceptor(new String[]{"/**"}, interceptor);
+//    }
 }
